dfc298c699c5dd6fe03d82619812af934ef6fa90
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-03-16 12:19:30 -0300

    update main flow and impornts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/'
 
-Vue.config.productionTip = false
+import 'jquery/dist/jquery.min'
+import 'vue-popperjs/dist/vue-popper.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+
+import './assets/css/style.css'
+
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
